2d553ea5680ce68a435555ee7cf8d2c9dd01d7c7
pnssharma <srivatsa.nagesh@gmail.com>
2019-06-22 22:16:48 +0530

    Linear search using recursive method

--- src/com/sharma/java/arrayproblems/SearchArray.java
+++ src/com/sharma/java/arrayproblems/SearchArray.java
@@ -14,6 +14,9 @@ public class SearchArray {
 		System.out.println("Value found : " + linearSearch(new Integer[] { 1, 3, 4, 5, 6, 7, 8, 9 }, 3));
 		System.out.println("Value found : " + linearSearch(new String[] { "Hello", "World", "Big", "bee" }, null));
 
+		System.out.println("Value found at index: "
+				+ recursiveLinearSearch(new String[] { "Hello", "World", "Big", "bee" }, 0, 3, "Big"));
+
 	}
 
 	/**
@@ -32,6 +35,26 @@ public class SearchArray {
 			}
 		}
 		return false;
+	}
+
+	/**
+	 * 
+	 * Recursive method to search element
+	 * 
+	 * @param array
+	 * @param l
+	 * @param r
+	 * @param value
+	 * @return returns index of found element
+	 */
+	private static <T> int recursiveLinearSearch(T[] array, int l, int r, T value) {
+		if (r < l)
+			return -1;
+		if (array[l] == value)
+			return l;
+		if (array[r] == value)
+			return r;
+		return recursiveLinearSearch(array, l + 1, r - 1, value);
 	}
 
 }
